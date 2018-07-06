faf0a9f563363f51c5441f059671b521c39d8300
dmhenry <davermont@gmail.com>
2018-07-06 15:40:23 -0400

    Move header comments to top of files.

--- ch1/1.04.rkt
+++ ch1/1.04.rkt
@@ -1,9 +1,9 @@
-#lang sicp
-
 ; Exercise 1.4
 ; Observe that our model of evaluation allows for combinations whose operators
 ; are compound expressions. Use this observation to describe the behavior of
 ; the following procedure:
+
+#lang sicp
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
