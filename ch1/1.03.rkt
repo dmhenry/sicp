9b613c371654337d83ce522d3ffb1ed9a7419503
dmhenry <davermont@gmail.com>
2018-07-06 15:36:21 -0400

    Remove shell script capability.

--- ch1/1.03.rkt
+++ ch1/1.03.rkt
@@ -1,7 +1,4 @@
-#!/usr/bin/env racket
-#lang racket
-(#%require sicp)
-(require racket/cmdline)
+#lang sicp
 
 ; Exercise 1.3
 ; Define a procedure that takes three numbers as arguments and returns the sum
@@ -16,12 +13,3 @@
     (cond ((and (<= x y) (<= x z)) (cons y z))
           ((and (<= y x) (<= y z)) (cons x z))
           (else (cons x y)))))
-
-; Run from shell
-(define parser
-  (command-line
-    #:program "sum-of-squares-larger-two"
-    #:args (x y z)
-    (printf "~a\n" (sum-of-squares-larger-two (string->number x)
-                                              (string->number y)
-                                              (string->number z)))))
--- ch1/1.05.rkt
+++ ch1/1.05.rkt
@@ -1,4 +1,3 @@
-#!/usr/bin/env racket
 #lang sicp
 
 ; Exercise 1.5
