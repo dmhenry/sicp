80bcc004959486f5597b5ae4bb6823b459d0a672
dmhenry <davermont@gmail.com>
2018-07-19 14:01:01 -0400

    Changed procedure order to reflect usage order.

--- ch1/1.06.rkt
+++ ch1/1.06.rkt
@@ -8,11 +8,11 @@
 (define (square x)
   (* x x))
 
-(define (improve guess x)
-  (average guess (/ x guess)))
-
 (define (average x y)
   (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
 
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
