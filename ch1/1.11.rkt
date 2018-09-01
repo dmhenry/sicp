21f37b027d08831c373c73e39f087e9b4fd5a7fa
dmhenry <davermont@gmail.com>
2018-09-01 17:02:49 -0400

    Handle negative inputs consistent with the rule.

--- ch1/1.11.rkt
+++ ch1/1.11.rkt
@@ -15,13 +15,13 @@
 
 (define (g n)
   (define (g-iter a b c count)
-    (if (= count 0)
-      c
-      (g-iter (+ a (* 2 b) (* 3 c))
-              a
-              b
-              (- count 1))))
-  (g-iter 2 1 0 n))
+    (cond ((= count 0) c)
+          ((< count 0) n)
+          (else (g-iter (+ a (* 2 b) (* 3 c)) 
+                        a
+                        b
+                        (- count 1)))))
+    (g-iter 2 1 0 n))
 
 ; Think of the iterative version as a sliding window that contains the three
 ; most recently computed values. The n-th value is the least recently
