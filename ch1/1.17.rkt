ac9bc65763d85e91cb8a861ddc2153f73c3d5f9c
dmhenry <davermont@gmail.com>
2018-10-06 15:34:59 -0400

    Change to recursive process solution with Θ(log₂(n)) space & time complexity.

--- ch1/1.17.rkt
+++ ch1/1.17.rkt
@@ -27,51 +27,41 @@
 (define (halve n)
   (/ n 2))
 
-(define (quick-mult a b)
-  (define (quick-mult-iter a b c)
-    (cond ((= b 0) c)
-          ((even? b) (quick-mult-iter (double a) (halve b) c))
-          ((> b 0) (quick-mult-iter a (- b 1) (+ c a)))
-          (else (quick-mult-iter a (+ b 1) (- c a)))))
-  (quick-mult-iter a b 0))
+(define (fast-mult a b)
+    (cond ((= b 0) 0)
+          ((even? b) (double (fast-mult a (halve b))))
+          ((> b 0) (+ a (fast-mult a (- b 1))))
+          (else (- (fast-mult a (+ b 1)) a))))
 
-; In the optimal case, the multiplicand b is a power of 2, which results in each
-; subsequent halving of b resulting in an even number until b=2. In these cases,
-; b=1 is the sole odd multiplicand encountered. These cases require sqrt(b)
-; doubling operations + 1 addition of c+a, when b=1. Observe that, in these
-; optimal cases, c=0 until b=1, at which point c=a and the process quickly
-; terminates. Hence, the asymptotic lower bound is Ω(log₂(n)). This is
-; demonstrated below with 5*16:
+; This process grows logarithmically with b in both space and number of steps.
+; Computing (fast-mult a 2b) requires only one more step than (fast-mult a b)
+; and one more deferred opteration, yielding a growth in both complexity and
+; space of Θ(log₂(n)). This is demonstrated below:
 ;
-; (quick-mult-iter 5 16 0)
-; (quick-mult-iter 10 8 0)
-; (quick-mult-iter 20 4 0)
-; (quick-mult-iter 40 2 0)
-; (quick-mult-iter 80 1 0)
-; (quick-mult-iter 80 0 80)
-;
-; In the worst case, each halving of the b is preceded by a subtraction, b=b-1
-; due to the presence of an odd integer multiplicand (b). (Note that this occurs
-; when b is one less than a power of 2.) This worst-case scenario effectively
-; doubles the number of steps involving changes to the values of a and c in the
-; invariant quantity ab+c (which holds until the base case is encountered). The
-; worst case, then, is represented by O(2log₂(n)) complexity growth. This is
-; demonstrated below with 5^15:
-;
-; (quick-mult-iter 5 15 0)
-; (quick-mult-iter 5 14 5)
-; (quick-mult-iter 10 7 5)
-; (quick-mult-iter 10 6 15)
-; (quick-mult-iter 20 3 15)
-; (quick-mult-iter 20 2 35)
-; (quick-mult-iter 40 1 35)
-; (quick-mult-iter 40 0 75)
-;
-; Because we are only interested in the most significant term in our complexity
-; growth, coefficients and logarithmic bases can be discarded, resulting in a 
-; general complexity of Θ(log(n)). The recursive quick-expt-iter procedure is
-; interpreted as an iterative process, which is to say that it results in a
-; linear recursive process, i.e., it is tail-recursive. This is because the
-; state of the process is captured entirely in its state variables (rather than
-; relying on a stack of deferred operations, as with a recursive process). For 
-; this reason, space complexity is constant in every case, i.e., Θ(1).
+; (fast-mult 3 -9)
+; (- (fast-mult 3 -8) 3)
+; (- (double (fast-mult 3 -4)) 3)
+; (- (double (double (fast-mult 3 -2))) 3)
+; (- (double (double (double (fast-mult 3 -1)))) 3)
+; (- (double (double (double (- (fast-mult 3 0) 3)))) 3)
+; (- (double (double (double (- 0 3)))) 3)
+; (- (double (double (double -3))) 3)
+; (- (double (double -6)) 3)
+; (- (double -12) 3)
+; (- -24 3)
+; -27
+
+; (fast-mult 3 -18)
+; (double (fast-mult 3 -9))
+; (double (- (fast-mult 3 -8) 3))
+; (double (- (double (fast-mult 3 -4)) 3))
+; (double (- (double (double (fast-mult 3 -2))) 3))
+; (double (- (double (double (double (fast-mult 3 -1)))) 3))
+; (double (- (double (double (double (- (fast-mult 3 0) 3)))) 3))
+; (double (- (double (double (double (- 0 3)))) 3))
+; (double (- (double (double (double -3))) 3))
+; (double (- (double (double -6)) 3))
+; (double (- (double -12) 3))
+; (double (- -24 3))
+; (double -27)
+; -54
