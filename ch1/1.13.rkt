8f31ffb1022dd1724952fb48f8f0fca27729da35
dmhenry <davermont@gmail.com>
2018-10-17 19:52:56 -0400

    Problem statement. TODO: inductive proof

---
+++ ch1/1.13.rkt
@@ -0,0 +1,5 @@
+; Exercise 1.13
+; Prove that Fib(n) is the closest integer to (ɸ^n)/√5, where ɸ=(1+√5)/2. Hint:
+; Let Ψ=(1-√5)/2. Use induction and the definition of the Fibonacci numbers (see
+; section 1.2.2) to prove that Fib(n) = (ɸ^n-Ψ^n)/√5.
+
